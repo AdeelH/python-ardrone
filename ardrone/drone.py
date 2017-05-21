bd90544421dd7e125bc33c5aaee985514b1dc0e1
Adeel <adeel_hassan93@hotmail.com>
2017-05-21 15:16:40 +0500

    experiment

--- ardrone/drone.py
+++ ardrone/drone.py
@@ -169,3 +169,15 @@ class ARDrone(object):
         va -- angular speed: float [-1..1] negative: spin left, positive: spin
             right"""
         self.at(ardrone.at.pcmd, True, lr, fb, vv, va)
+
+    def move2(self, lr, fb, vv, va):
+        """Makes the drone move (translate/rotate).
+
+        Parameters:
+        lr -- left-right tilt: float [-1..1] negative: left, positive: right
+        fb -- front-back tilt: float [-1..1] negative: forwards, positive:
+            backwards
+        vv -- vertical speed: float [-1..1] negative: go down, positive: rise
+        va -- angular speed: float [-1..1] negative: spin left, positive: spin
+            right"""
+        self.at(ardrone.at.pcmd, False, lr, fb, vv, va)
